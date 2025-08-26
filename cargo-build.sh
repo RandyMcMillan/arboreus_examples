0cde333cb565c58ca7ee7114c681dd786cebccee
randymcmillan <randymcmillan@protonmail.com>
2025-08-26 12:30:39 -0400

    cargo-build.sh:symlinking

--- cargo-build.sh
+++ cargo-build.sh
@@ -12,10 +12,14 @@ if [ "$1" == "--clean" ]; then
         cargo clean --manifest-path $project;\
     done
 fi
-
-#clean all target dirs
-#for project in $(ls --color=never **/*/*/*/Cargo.toml);do echo $project;sleep 0;cargo clean --manifest-path $project;done
-for project in $PROJECTS;do this_project=$(echo $project|sed 's/\/Cargo.toml//g');echo "this=$this_project"; ln -s $TARGET $this_project/;ls -F $this_project; done
+if [ "$1" == "--sleep" ]; then
+if [ "$2" != "" ]; then
+    SLEEP=$2
+else
+    SLEEP=0
+fi
+fi
+for project in $PROJECTS;do this_project=$(echo $project|sed 's/\/Cargo.toml//g');echo "this=$this_project"; sleep $SLEEP;ln -shf $TARGET $this_project;ls -F $this_project; done
 #for project in $PROJECTS;do this_project=$(echo $project|sed 's/Cargo.toml//g');echo "this=$this_project"; mkdir -p $this_project/target;ls -F $this_project; done
 #exit;
-for project in $(ls --color=never **/*/*/*/Cargo.toml);do echo $project;sleep 0;cargo b --manifest-path $project;done
+for project in $(ls --color=never **/*/*/*/Cargo.toml);do echo $project;sleep $SLEEP;cargo b --manifest-path $project;done
